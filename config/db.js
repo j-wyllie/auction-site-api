7395d9dac40848f80f7e3354ae647b9c4ca8805c
Joshua <jwy31@uclive.ac.nz>
2018-03-11 17:48:22 +1300

    Set up folder structure and created empty files

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+let state = {
+    pool: null
+};
+
+exports.connect = function(done) {
+    state.pool = mysql.createPool({
+        host: 'mysql3.csse.canterbury.ac.nz',
+        user: 'jwy31',
+        password: "62376261",
+        database: "jwy31"
+    });
+    done();
+};
+
+exports.get_pool = function() {
+    return state.pool;
+};
---
+++ config/express.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+
+module.exports = function() {
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    require('../app/routes/users.server.routes.js')(app);
+
+    return app;
+};
